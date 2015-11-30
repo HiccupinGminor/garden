1d2af18eece4910dc742de3d7389ba99b1f364f6
Chris Chung <hiccupinGminor@gmail.com>
2015-11-29 22:16:54 -0800

    Refactor logic into socket-accessible router file

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 var express = require('express');
 var app = express();
-var router = require('./server/router.js');
+// var router = require('./server/router.js');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+var routes = require('./server/router');
 mongoose.Promise = require('bluebird');
 
 var socketServer = require('http').Server(app);
@@ -21,28 +22,7 @@ db.on('error', console.log);
 db.on('disconnected', connect);
 
 app.use(bodyParser.json());
-app.use('/api/v1', router);
 app.use(express.static('public'));
 app.listen(3000);
 
-var Reading = require('./server/models/Reading');
-
-app.post('/api/v1/readings', function(req, res) {
-	var body = req.body,
-			model;
-		
-	model = new Reading(body);
-	model.save()
-	.then(function(data) {
-		io.emit('readings');
-		res.status(201).send(data);
-	})
-	.catch(function(error) {
-		if(error.name == 'ValidationError') {
-			res.sendStatus(400);
-		}
-		else {
-			res.sendStatus(500);
-		}
-	});
-});
+routes(app, io);
--- server/router.js
+++ server/router.js
@@ -1,8 +1,53 @@
 "use strict";
 
-var express = require('express');
-var ReadingController = require('./controllers/ReadingController');
-var router = express.Router();
+var Reading = require('./models/Reading');
+var ErrorHandler = require('./utilities/ErrorHandler');
+var baseUrl = '/api/v1';
 
-router.use('/readings', ReadingController);
-module.exports = router;
+module.exports = function routes(app, io) {
+
+	app.post(baseUrl + '/readings', function(req, res) {
+		var body = req.body,
+				model;
+			
+		model = new Reading(body);
+		model.save()
+		.then(function(data) {
+			io.emit('readings');
+			res.status(201).send(data);
+		})
+		.catch(function(error) {
+			if(error.name == 'ValidationError') {
+				res.sendStatus(400);
+			}
+			else {
+				res.sendStatus(500);
+			}
+		});
+	});
+
+	app.get(baseUrl + '/readings', function(req, res) {
+		Reading.find().exec()
+			.then(function(data) {
+				res.send(data);
+			})
+			.catch(ErrorHandler);
+	});
+
+	app.get(baseUrl + '/readings/:id?', function(req, res) {
+		Reading
+			.findById(req.params.id)
+			.exec()
+			.then(function(data) {
+				res.send(data);
+			})
+			.catch(function(error) {
+				if(error.name == 'CastError') {
+					res.sendStatus(404);
+				}
+				else {
+					res.sendStatus(500);
+				}
+			});
+	});
+}
