b3a5296e2be97d617cccc0209a0c5e6c2d02978a
Chris Chung <hiccupinGminor@gmail.com>
2015-12-10 11:51:25 -0800

    Run bundle task initially with gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 
 gulp.task('bundle', function() {
-    gulp.src('public/src/main.jsx', { read: false })
+    gulp.src('public/src/*.jsx', { read: false })
       .pipe(browserify({
       	transform: ['reactify'],
       	extensions: ['.jsx'],
@@ -12,6 +12,10 @@ gulp.task('bundle', function() {
       .pipe(gulp.dest('./public/dist/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['public/src/main.jsx'], ['bundle']);
+gulp.task('watch-bundle', function() {
+	if(gulp.tasks.bundle) { 
+    gulp.start('bundle');
+	}
+
+  gulp.watch(['public/src/*.jsx'], ['bundle']);
 });
