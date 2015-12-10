ef3be3b138d4e41bc5b3530c2932be58b2f96f94
Chris Chung <hiccupinGminor@gmail.com>
2015-12-10 11:57:22 -0800

    Add api tests to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
 
 gulp.task('bundle', function() {
-    gulp.src('public/src/*.jsx', { read: false })
-      .pipe(browserify({
-      	transform: ['reactify'],
-      	extensions: ['.jsx'],
-      }))
-      .pipe(rename('include.js'))
-      .pipe(gulp.dest('./public/dist/'));
+  gulp.src('public/src/*.jsx', { read: false })
+    .pipe(browserify({
+    	transform: ['reactify'],
+    	extensions: ['.jsx'],
+    }))
+    .pipe(rename('include.js'))
+    .pipe(gulp.dest('./public/dist/'));
+});
+
+gulp.task('test-server', function() {
+	return gulp.src('tests/*.js')
+	.pipe(mocha());
 });
 
 gulp.task('watch-bundle', function() {
