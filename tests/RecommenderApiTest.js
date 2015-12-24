2bb4bdf9bd0f065378a8d81d2a04cff7adb6a09d
Chris Chung <hiccupinGminor@gmail.com>
2015-12-24 15:19:03 -0800

    Upgrade tests to cover failing non-valid zip code case for recommendation resource

--- tests/RecommenderApiTest.js
+++ tests/RecommenderApiTest.js
@@ -2,21 +2,31 @@
 
 var request = require('supertest');
 var baseUrl = 'http://localhost:3000';
-
+var baseRoute = '/api/v1/recommendations';
 describe('Recommender API', function() {
-
 	describe('GET collection', function() {
 		it('Should return a 400 without zip code', function(done) {
 			request(baseUrl)
-				.get('/api/v1/recommendations')
+				.get(baseRoute)
 				.expect(400, done);
 		});
 
-		it('Should return json with a 200 with zip code', function(done) {
+		it('Should return json array with a 200 with zip code', function(done) {
 			request(baseUrl)
-				.get('/api/v1/recommendations?zip=90000')
+				.get(baseRoute + '?zip=90004')
 				.expect('Content-Type', 'application/json; charset=utf-8')
+				.expect(function(res) {
+					if(!Array.isArray(res.body)) {
+						throw new Error("Body should be an array");
+					}
+				})
 				.expect(200, done);
+		});
+
+		it('Should return a 400 with a bogus zip code', function(done) {
+			request(baseUrl)
+				.get(baseRoute + '?zip=notrealzip')
+				.expect(400, done);
 		});
 	});
 });
