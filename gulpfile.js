a56ff1b1c7ec8eb536f835568a8cdbec5ec826bd
Chris Chung <hiccupinGminor@gmail.com>
2015-12-30 08:50:55 -0800

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+"use strict";
+
 const gulp = require('gulp');
 const browserify = require('gulp-browserify');
 const rename = require('gulp-rename');
 const mocha = require('gulp-mocha');
+const livereload = require('gulp-livereload');
 
 gulp.task('bundle', function() {
   gulp.src('public/src/*.jsx', { read: false })
@@ -10,7 +13,8 @@ gulp.task('bundle', function() {
     	extensions: ['.jsx'],
     }))
     .pipe(rename('include.js'))
-    .pipe(gulp.dest('./public/dist/'));
+    .pipe(gulp.dest('./public/dist/'))
+    .pipe(livereload({ start: true }));
 });
 
 gulp.task('test-server', function() {
@@ -23,5 +27,6 @@ gulp.task('watch-bundle', function() {
     gulp.start('bundle');
 	}
 
+  livereload.listen();
   gulp.watch(['public/src/*.jsx'], ['bundle']);
 });
